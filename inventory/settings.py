1b70834306dd97f8b763c450e5d92de25df2efc8
liulixiang1988 <liulixiang1988@gmail.com>
2014-09-02 08:35:52 +0800

    initial

---
+++ inventory/settings.py
@@ -0,0 +1,88 @@
+"""
+Django settings for inventory project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '93+pc_zx44m^4&kf66_f+ci@l1^$$z@7gn%!oo(!n8_e@jor2a'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'inventory_app',
+    'south',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'inventory.urls'
+
+WSGI_APPLICATION = 'inventory.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sqlserver_ado',
+        'NAME': 'data_center',
+        'USER': 'sa',
+        'PASSWORD': 'tlys.oaxmz.5860247',
+        'HOST': 'localhost',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'zh-CN'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ inventory_app/models.py
@@ -0,0 +1,80 @@
+# -*- coding:utf-8 -*-
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+# * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Remove `managed = False` lines if you wish to allow Django to create and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Inventory(models.Model):
+    id = models.sqlserver_ado.fields.BigAutoField(primary_key=True)
+    inventory_code = models.CharField(max_length=255, blank=True)
+    inventory_name = models.CharField(max_length=255, blank=True)
+    remark = models.CharField(max_length=255, blank=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    create_user_id = models.CharField(max_length=255, blank=True)
+    create_user_name = models.CharField(max_length=255, blank=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+    update_user_id = models.CharField(max_length=255, blank=True)
+    update_user_name = models.CharField(max_length=255, blank=True)
+    is_available = models.SmallIntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'inventory'
+
+
+class MaterialInventory(models.Model):
+    id = models.sqlserver_ado.fields.BigAutoField(primary_key=True)
+    item_code = models.CharField(max_length=255, blank=True)
+    inventory_code = models.CharField(max_length=255, blank=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    create_user_id = models.CharField(max_length=255, blank=True)
+    create_user_name = models.CharField(max_length=255, blank=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+    update_user_id = models.CharField(max_length=255, blank=True)
+    update_user_name = models.CharField(max_length=255, blank=True)
+    is_available = models.SmallIntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'material__inventory'
+
+
+class AnalysisResults(models.Model):
+    id = models.AutoField()
+    sample_code = models.CharField(max_length=50, blank=True)
+    sample_sub_id = models.CharField(max_length=50, blank=True)
+    sample_date = models.DateTimeField(blank=True, null=True)
+    sample_name = models.CharField(max_length=50, blank=True)
+    sample_user_name = models.CharField(max_length=50, blank=True)
+    sample_user_id = models.CharField(max_length=50, blank=True)
+    mode = models.CharField(max_length=50, blank=True)
+    burn = models.CharField(max_length=50, blank=True)
+    element_name = models.CharField(max_length=50, blank=True)
+    element_code = models.CharField(max_length=50, blank=True)
+    element_value = models.CharField(max_length=50, blank=True)
+    element_unit = models.CharField(max_length=20, blank=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    create_user_code = models.CharField(max_length=50, blank=True)
+    create_user_name = models.CharField(max_length=50, blank=True)
+    is_in_use = models.NullBooleanField()
+    used_by = models.CharField(max_length=50, blank=True)
+    remark = models.CharField(max_length=50, blank=True)
+    attribute1 = models.CharField(max_length=50, blank=True)
+    attribute2 = models.CharField(max_length=50, blank=True)
+    attribute3 = models.CharField(max_length=50, blank=True)
+    attribute4 = models.CharField(max_length=50, blank=True)
+    attribute5 = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_analysis_results'
+
